81e4f711b18ccc27040e1151c0fd719dc7f19cce
Sky Rolnick <skymeson@gmail.com>
2017-06-26 19:35:24 -0400

    Update after Dev Day
    Basic clean up, homework is to add a GameManager class as a singleton
    and an event system that listens for the player to die and callse Game
    Over.

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyManager : MonoBehaviour {
+
+
+    public Slider enemy_health;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        //Debug.Log("Collision with object detected.  " + collision.gameObject.name);
+
+        if ( collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision with player detected.");
+            enemy_health.value -= 15;
+
+        }
+
+        if(enemy_health.value <=0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+
+    }
+}
